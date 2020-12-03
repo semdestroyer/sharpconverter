b80f6df91f19a22d21f1816cb78eb1c561020ebc
semdestroyer <semdestroyer@yandex.ru>
2020-12-03 20:27:15 +0200

    Добавьте файлы проекта.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(156, 68);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(389, 67);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "в 10",
+            "в 16"});
+            this.comboBox1.Location = new System.Drawing.Point(262, 67);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(281, 158);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Перевести";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "из 16",
+            "из 10"});
+            this.comboBox2.Location = new System.Drawing.Point(29, 68);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(781, 454);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox2;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string s;
+            
+
+
+            textBox2.Text = comboBox2.GetItemText(comboBox2.SelectedItem);
+            if (comboBox2.GetItemText(comboBox2.SelectedItem) == "из 10")
+            {
+                s = "dec";
+                
+
+            }
+            if (comboBox2.GetItemText(comboBox2.SelectedItem) == "из 16")
+            {
+                s = "hex";
+                
+            }
+            textBox2.Text = comboBox1.GetItemText(comboBox1.SelectedItem);
+            if (comboBox1.GetItemText(comboBox1.SelectedItem) == "в 10")
+            {
+                textBox2.Text = toDec(textBox1.Text).ToString();
+
+            }
+            if (comboBox1.GetItemText(comboBox1.SelectedItem) == "в 16")
+            {
+                textBox2.Text = toHex(int.Parse(textBox1.Text));
+            }
+           
+            
+        }
+        private string toHex(int dec) 
+        {
+            string hex = "";
+            while (dec != 0)
+            {
+                int g = dec % 16;
+                switch (g)
+                {
+                    case 0:
+                        hex += "0";
+                        break;
+                    case 1: hex += "1"; 
+                        break;
+                    case 2:
+                        hex += "2";
+                        break;
+                    case 3:
+                        hex += "3";
+                        break;
+                    case 4:
+                        hex += "4";
+                        break;
+                    case 5:
+                        hex += "5";
+                        break;
+                    case 6:
+                        hex += "6";
+                        break;
+                    case 7:
+                        hex += "7";
+                        break;
+                    case 8:
+                        hex += "8";
+                        break;
+                    case 9:
+                        hex += "9";
+                        break;
+                    case 10:
+                        hex += "A";
+                        break;
+                    case 11:
+                        hex += "B";
+                        break;
+                    case 12:
+                        hex += "C";
+                        break;
+                    case 13:
+                        hex += "D";
+                        break;
+                    case 14:
+                        hex += "E";
+                        break;
+                    case 15:
+                        hex += "F";
+                        break;
+                }
+
+                dec = dec / 16;
+            }
+            
+            return reverse(hex);
+        }
+        private int toDec(String hex)
+        {
+            
+            hex = reverse(hex);
+            char[] hx = hex.ToCharArray();
+            int outdec = 0;
+            int dec = 0;
+            for (int i = 0;i < hex.Length; i++)
+            {
+                
+                switch (hx[i])
+                {
+                    case '0':
+                        dec = 0;
+                        break;
+                    case '1':
+                        dec = 1;
+                        break;
+                    case '2':
+                        dec = 2;
+                        break;
+                    case '3':
+                        dec = 3;
+                        break;
+                    case '4':
+                        dec = 4;
+                        break;
+                    case '5':
+                        dec = 5;
+                        break;
+                    case '6':
+                        dec = 6;
+                        break;
+                    case '7':
+                        dec = 7;
+                        break;
+                    case '8':
+                        dec = 8;
+                        break;
+                    case '9':
+                        dec = 9;
+                        break;
+                    case 'A':
+                        dec = 10;
+                        break;
+                    case 'B':
+                        dec = 11;
+                        break;
+                    case 'C':
+                        dec = 12;
+                        break;
+                    case 'D':
+                        dec = 13;
+                        break;
+                    case 'E':
+                        dec = 14;
+                        break;
+                    case 'F':
+                        dec = 15;
+                        break;
+                }
+
+                outdec += (int)(dec *  Math.Pow(16,i));
+                Console.WriteLine(dec * Math.Pow(16, i));
+            }
+
+            return outdec;
+        }
+        string reverse(string s)
+        {
+            
+            char[] newstring = new char[s.Length];
+
+            for (int i = 0; i<s.Length ;i++)
+            {
+                newstring[i] = s[s.Length - i - 1];
+                Console.WriteLine(i);
+            }
+        
+            return new string(newstring);
+        }
+
+
+
+
+    }
+}
